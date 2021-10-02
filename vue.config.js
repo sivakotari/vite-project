def9192354ca50c78d40b0484ebb6213f9b873be
sivakotari <thnkdifff@gmail.com>
2021-10-02 15:33:28 +0530

    first commit

---
+++ vue.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  // css: {
+  //   loaderOptions: {
+  //     scss: {
+  //       data: `@import '~/styles/index.scss';`
+  //     }
+  //   }
+  // }
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       data: `@import '~/styles/index.scss';`
+  //     }
+  //   }
+  // }
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //   },
+  // },
+  // css: {
+  //   loaderOptions: {
+  //     scss: {
+  //       // data: `@import "~@/styles/index.scss";`
+  //       // prependData: `@import "~@/styles/index.scss";`
+  //       additionalData: `@import "@/styles/index.scss";`
+  //       // imports: [path.resolve(__dirname, `src/styles/index.scss`)],
+  //     }
+  //   }
+  // }
+  // transpileDependencies: [
+  //   'vuetify'
+  // ]
+}
